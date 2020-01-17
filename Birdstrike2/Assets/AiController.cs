3a4b7ae30d8dbdd1621ffc232859f8fc7db79a62
Zphill15 <designsargo@gmail.com>
2020-01-17 17:29:20 -0500

    Init
    Added initial Agent scene with obstacles

---
+++ Birdstrike2/Assets/AiController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AiController : MonoBehaviour
+{
+
+    NavMeshAgent agent;
+    public GameObject goal;
+
+    private void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+        agent.SetDestination(goal.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
