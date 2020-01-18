674dfe691643141ca1cd9d8da845053082187baa
haitheredavid <30870056+haitheredavid@users.noreply.github.com>
2020-01-17 20:02:33 -0500

    added nreal

---
+++ Birdstrike2/Assets/dTestField/MeshGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class MeshGenerator : MonoBehaviour {
+
+    public GameObject start;
+    public GameObject end;
+//grab two points
+    private float3 _start;
+    private float3 _end;
+
+    void Start( ) {
+        
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update( ) {
+
+
+        _start = start.transform.position;
+        _end = end.transform.position;
+        
+        
+        
+    }
+
+}
