36b0dfb8d2fe3359d7aa81a472641d87ff57b06c
snaumovski <snaumovski@users.noreply.github.com>
2020-01-18 22:25:23 -0500

    messed with slider

---
+++ Birdstrike2/Assets/myScripts/ShowValueScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowValueScript : MonoBehaviour
+{
+    
+
+
+    public void SetTextOfCount(int value)
+    {
+        var percentageText = GetComponent<Text>();
+
+        percentageText.text = Mathf.RoundToInt(value * 100).ToString();
+    }
+}
